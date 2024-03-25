6d4ced1526dd74b114742e9fd7544d73e4eab304
Ali <Alwer.design@gmail.com>
2024-03-25 14:38:59 +0330

    Change Saved Track Design

--- Frontend/src/components/home components/libraryTrack.vue
+++ Frontend/src/components/home components/libraryTrack.vue
@@ -2,21 +2,77 @@
   <div id="parent" class="container mt-5 px-3 position-relative">
     <section>
       <div>
-        <div class="d-flex justify-content-between p">
+        <div class="d-flex justify-content-between">
           <span class="text-uppercase color-blue fw-semibold fs-5"
             >Saved tracks</span
           >
         </div>
-        <div class="d-flex justify-content-start mt-4 gap-3 flex-wrap">
-          <div class="track-body d-flex">
+        <div>
+          <div class="d-flex align-items-center px-3 gap-4 mt-3">
             <img
               src="../../assets/img/test/eminem.jpg"
-              class="img-fluid rounded-4"
+              class="tumbnail img-fluid rounded-4"
               alt=""
             />
-            <div class="track-body-text-parent d-flex justify-content-center">
-              <p class="text-center">Eminem</p>
+            <div class="d-flex flex-column text-div gap-2 ">
+              <span class="text-capitalize color-black fw-bold fs-5"
+                >Track</span
+              >
+              <span class="text-capitalize color-gray">Artist</span>
             </div>
+            <span class="color-gray">3:17</span>
+            <div>
+              <img src="../../assets/icons/like.png"  alt="">
+              <!-- <img src="../../assets/icons/unlike.png"  alt=""> -->
+
+
+              
+            </div>
+            <svg
+            class="more"
+            width="18"
+            height="6"
+            viewBox="0 0 18 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="3" cy="3" r="3" fill="#B3B3BC" />
+            <circle cx="15" cy="3" r="3" fill="#B3B3BC" />
+          </svg>
+            
+          </div>
+          <div class="d-flex align-items-center px-3 gap-4 mt-3">
+            <img
+              src="../../assets/img/test/eminem.jpg"
+              class="tumbnail img-fluid rounded-4"
+              alt=""
+            />
+            <div class="d-flex flex-column text-div gap-2 ">
+              <span class="text-capitalize color-black fw-bold fs-5"
+                >Track</span
+              >
+              <span class="text-capitalize color-gray">Artist</span>
+            </div>
+            <span class="color-gray">3:17</span>
+            <div>
+              <!-- <img src="../../assets/icons/like.png"  alt=""> -->
+              <img src="../../assets/icons/unlike.png"  alt="">
+
+
+              
+            </div>
+            <svg
+            class="more"
+            width="18"
+            height="6"
+            viewBox="0 0 18 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="3" cy="3" r="3" fill="#B3B3BC" />
+            <circle cx="15" cy="3" r="3" fill="#B3B3BC" />
+          </svg>
+            
           </div>
         </div>
       </div>
@@ -40,28 +96,22 @@ export default {
   height: 80vh;
   overflow: hidden !important;
 }
-.track-body {
-  position: relative;
-  width: 160px;
-  height: 160px;
+.tumbnail {
+  width: 70px;
+  height: 70px;
 }
-.track-body-text-parent {
-  position: absolute;
-  bottom: -5%;
-  font-weight: bold;
-  width: 100%;
+.text-div {
+  width: 700px;
 }
-.track-body-text-parent p {
-  color: white;
-
-  z-index: 5;
+.more{
+  transform: rotate(90deg);
+  cursor: pointer;
 }
 
 .pagination {
   position: absolute;
-  bottom:0%;
+  bottom: 0%;
   display: flex;
   justify-content: center;
-  
 }
 </style>
--- Frontend/src/components/home.vue
+++ Frontend/src/components/home.vue
@@ -42,7 +42,7 @@ export default {
   name: "home",
   data() {
     return {
-      component: "laibraryartist",
+      component: "laibrarytrack",
     };
   },
   methods: {},
