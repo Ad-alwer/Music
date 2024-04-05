56182373ca92948e1a9d7ddb44d25de540735d41
Ali <Alwer.design@gmail.com>
2024-04-05 18:29:00 +0330

    Update User profile Design

--- Frontend/src/components/home components/user.vue
+++ Frontend/src/components/home components/user.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="parent" class="container mt-4 mx-3 d-flex">
     <div class="w-75 px-2 main">
-      <div class="info d-flex gap-3">
+      <div class="info d-flex justify-content-center gap-5">
         <div class="profile-parent d-flex gap-3 align-items-center">
           <div class="">
             <img
@@ -25,7 +25,9 @@
           </div>
         </div>
       </div>
-      <p class="mt-2 color-gray bio">hi,there i am eminem</p>
+      <div class="d-flex justify-content-center ms-5">
+        <p class="mt-2 color-gray bio">hi,there i am eminem</p>
+      </div>
       <div class="px-2">
         <div class="d-flex justify-content-between">
           <span
@@ -60,8 +62,24 @@
             "
             >playlist</span
           >
+          <span
+            :class="
+              component == 'following'
+                ? 'text-capitalize p-2 pointer fw-semibold active'
+                : 'text-capitalize p-2 pointer fw-semibold'
+            "
+            >following</span
+          >
+          <span
+            :class="
+              component == 'followers'
+                ? 'text-capitalize p-2 pointer fw-semibold active'
+                : 'text-capitalize p-2 pointer fw-semibold'
+            "
+            >followers</span
+          >
         </div>
-        <div v-if="!podcast && !album" class="px-4">
+        <div v-if="!detail" class="px-4">
           <div
             class="monthly-box d-flex gap-4 align-items-center justify-content-between mt-3"
           >
@@ -71,46 +89,13 @@
                 src="../../assets/img/test/eminem.jpg"
                 alt=""
               />
-              <div class="d-flex flex-column monthly-text">
-                <p
-                  class="text-capitalize fs-5 fw-bold m-0 p-0 color-dark pointer"
-                >
-                  track
-                </p>
-              </div>
-            </div>
-            <div class="d-flex gap-4 align-items-center">
-              <span class="me-3 text-capitalize">3:15 </span>
-              <span class="me-3 text-capitalize">25k plays</span>
-              <img
-                v-if="component == 'podcast' || component == 'album'"
-                src="../../assets/icons/greater-than.png"
-                class="img-fluid img-greater-than pointer"
-                alt=""
-              />
-              <svg
-                class="pointer svg-more"
-                width="18"
-                height="6"
-                viewBox="0 0 18 6"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+              <div
+                :class="
+                  detail
+                    ? 'd-flex flex-column album-monthly-text'
+                    : 'd-flex flex-column monthly-text'
+                "
               >
-                <circle cx="3" cy="3" r="3" fill="#B3B3BC" />
-                <circle cx="15" cy="3" r="3" fill="#B3B3BC" />
-              </svg>
-            </div>
-          </div>
-          <div
-            class="monthly-box d-flex gap-4 align-items-center justify-content-between mt-3"
-          >
-            <div class="d-flex gap-5 align-items-center">
-              <img
-                class="monthly-img img-fluid rounded-3"
-                src="../../assets/img/test/eminem.jpg"
-                alt=""
-              />
-              <div class="d-flex flex-column monthly-text">
                 <p
                   class="text-capitalize fs-5 fw-bold m-0 p-0 color-dark pointer"
                 >
@@ -118,16 +103,64 @@
                 </p>
               </div>
             </div>
-            <div class="d-flex gap-4 align-items-center">
+            <div class="d-flex gap-3 align-items-center">
               <span class="me-3 text-capitalize">3:15 </span>
               <span class="me-3 text-capitalize">25k plays</span>
-              <img
-                v-if="component == 'podcast' || component == 'album'"
-                src="../../assets/icons/greater-than.png"
-                class="img-fluid img-greater-than pointer"
-                alt=""
-              />
+              <span class="me-3 text-capitalize">2k likes</span>
 
+              <img
+                v-if="detail"
+                src="../../assets/icons/greater-than.png"
+                class="img-fluid img-greater-than pointer"
+                alt=""
+              />
+              <svg
+                class="pointer svg-more"
+                width="18"
+                height="6"
+                viewBox="0 0 18 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="3" cy="3" r="3" fill="#B3B3BC" />
+                <circle cx="15" cy="3" r="3" fill="#B3B3BC" />
+              </svg>
+            </div>
+          </div>
+          <div
+            class="monthly-box d-flex gap-4 align-items-center justify-content-between mt-3"
+          >
+            <div class="d-flex gap-5 align-items-center">
+              <img
+                class="monthly-img img-fluid rounded-3"
+                src="../../assets/img/test/eminem.jpg"
+                alt=""
+              />
+              <div
+                :class="
+                  detail
+                    ? 'd-flex flex-column album-monthly-text'
+                    : 'd-flex flex-column monthly-text'
+                "
+              >
+                <p
+                  class="text-capitalize fs-5 fw-bold m-0 p-0 color-dark pointer"
+                >
+                  track
+                </p>
+              </div>
+            </div>
+            <div class="d-flex gap-3 align-items-center">
+              <span class="me-3 text-capitalize">3:15 </span>
+              <span class="me-3 text-capitalize">25k plays</span>
+              <span class="me-3 text-capitalize">2k likes</span>
+
+              <img
+                v-if="detail"
+                src="../../assets/icons/greater-than.png"
+                class="img-fluid img-greater-than pointer"
+                alt=""
+              />
               <svg
                 class="pointer svg-more"
                 width="18"
@@ -142,7 +175,42 @@
             </div>
           </div>
         </div>
-        <div v-if="podcast || album">
+        <div
+          v-else-if="
+            detail && (component == 'following' || component == 'followers')
+          "
+          class="d-flex flex-column gap-2"
+        >
+          <div class="mt-2 px-4 d-flex justify-content-between mx-4 align-items-center">
+            <div class="d-flex gap-4 align-items-center">
+              <img
+                src="../../assets/img/test/eminem.jpg"
+                alt=""
+                class="img-fluid img-follow rounded-circle"
+              />
+              <span class="text-capitalize fw-bold fs-5">eminem</span>
+            </div>
+            <div class="d-flex justify-content-center">
+              <button class="btn px-3 py-2 btn-follow text-white fw-semibold text- text-capitalize">follow</button>
+              <!-- <button class="btn px-3 py-2 bg-danger  text-white fw-semibold text- text-capitalize">unfollow</button> -->
+            </div>
+          </div>
+          <div class="mt-2 px-4 d-flex justify-content-between mx-4 align-items-center">
+            <div class="d-flex gap-4 align-items-center">
+              <img
+                src="../../assets/img/test/eminem.jpg"
+                alt=""
+                class="img-fluid img-follow rounded-circle"
+              />
+              <span class="text-capitalize fw-bold fs-5">eminem</span>
+            </div>
+            <div class="d-flex justify-content-center">
+              <button class="btn px-3 py-2 btn-follow text-white fw-semibold text- text-capitalize">follow</button>
+              <!-- <button class="btn px-3 py-2 bg-danger  text-white fw-semibold text- text-capitalize">unfollow</button> -->
+            </div>
+          </div>
+        </div>
+        <div v-else>
           <div
             class="monthly-box d-flex gap-4 align-items-center justify-content-between mt-3"
           >
@@ -285,16 +353,14 @@ export default {
   name: "user",
   data() {
     return {
-      component: "podcast",
-      podcast: true,
-      album: false,
+      component: "followers",
+      detail: true,
     };
   },
 };
 </script>
 
 <style scoped>
-
 .profile-parent img {
   width: 110px;
   height: 110px;
@@ -335,22 +401,27 @@ export default {
   height: 50px;
 }
 .monthly-text {
-  width: 250px;
+  width: 220px;
   text-align: left !important;
 }
 
-.monthly-text p {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  max-width: 120px;
+.album-.monthly-text {
+  width: 200px;
+  text-align: left !important;
 }
 
 .svg-more {
   transform: rotate(90deg);
 }
-.img-greater-than{
+.img-greater-than {
   width: 20px;
   opacity: 0.8;
+}
+.img-follow {
+  width: 60px;
+}
+.btn-follow{
+  max-height: 40px;
+  background-color: var(--blue-main);
 }
 </style>
