5f42eed7f859c1ef6f3cd4b7055f3c7a57cf0f54
Ali <Alwer.design@gmail.com>
2024-07-27 23:25:10 +0330

    Add npm start

--- Backend/Routes/AccountspendingRoutes.js
+++ Backend/Routes/AccountspendingRoutes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const Router = express.Router();
 
 const accountspendingDB = require("../Databases/AccountsPending");
-const usersDB = require("../Databases/users");
-
+const usersDB = require("../Databases/Users");
 Router.get("/checkmail/:value", (req, res) => {
   usersDB.checkemail(req.params.value).then((data) => res.send(data));
 });
@@ -19,7 +18,6 @@ Router.post("/register", (req, res) => {
     .adduser(...Object.values(req.body))
     .then((data) => res.send(data));
 });
-
 
 Router.get("/verify", (req, res) => {
   accountspendingDB.verify(req.headers.jwt).then((data) => res.send(data));
